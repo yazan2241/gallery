6c8d0718d0ab8e58a58afb305fdd30fa994815da
Yazan <yazansy097@gmail.com>
2024-07-29 04:38:50 +0300

    Update AttributeApiController.php

--- app/Http/Controllers/Api/AttributeApiController.php
+++ app/Http/Controllers/Api/AttributeApiController.php
@@ -44,37 +44,8 @@ class AttributeApiController extends Controller
     }
     }
 
-    public function uploadImageToAttribute (Request $request) {
-
-        $image = new Image();
-
-        if ($file = $request->file('image')) {
-            
-            storeImage($file , $request->id , $image);
-
-        } else {
-
-            return Response::json(
-                ['error' => 'please choose image first'],
-                201
-            );
-        }
-
-        $image->attribute_id = $request->id;
-        $image->save();
-
-        if ($image) {
-            return Response::json(
-                $image,
-                200
-            );
-        } else {
-            return Response::json(
-                ['error' => 'no images found'],
-                201
-            );
-        }
-    }
+    
+    
 
     
 }
